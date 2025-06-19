77d7ac645d674b494d1103f61e5e1572bc04aaf1
justart-dev <hbd9425@gmail.com>
2025-06-19 23:22:37 +0900

    패치 API 수정 및 배치 작업 준비 테스트

--- app/api/steam-patch-test/dummy-data.ts
+++ app/api/steam-patch-test/dummy-data.ts
@@ -1,29 +1,74 @@
-import bbobHTML from "@bbob/html";
-import presetHTML5 from "@bbob/preset-html5";
-
-const row =
-  "[p]Greetings, Rivals![/p][p][/p][p]We're thrilled to announce that the upcoming patch drops on June 12, 2025, at 09:00:00 (UTC)! As usual, this update will be applied without server downtime—so you can dive right back into your epic battles as soon as the update is live![/p][p] [/p][p]Here's a look at what's coming in this patch:[/p][p] [/p][h1][b]616 Day Vault[/b][/h1][p][/p][p]To celebrate 616 Day and the larger Marvel Universe, we're launching the inaugural 616 Vault event on June 13th![/p][p]For one week only, enjoy the return of exclusive limited-time bundles:[/p][p]· Venom - Snow Symbiote Bundle[/p][p]· Magik - Frozen Demon Bundle[/p][p]· Rocket Raccoon - Wild Winter Bundle[/p][p]· Groot - Holiday Happiness Bundle[/p][p]· Hawkeye - Galactic Fangs Bundle[/p][p]· Captain America - Galactic Talon Bundle[/p][p]Event Duration: 2025/6/13 02:00:00 (UTC) ~ 2025/6/20 02:00:00 (UTC)[/p][p] [/p][h1][b]New In Store[/b][/h1][p][/p][p]1.  Invisible Woman - Future Foundation Bundle[/p][p]2. The Thing - Future Foundation Bundle[/p][p]Available from 2025/6/13, 02:00:00 (UTC)[/p][p] [/p][h1][b]Fixes[/b][/h1][h2][b]All Platforms[/b][/h2][p][/p][p]1. Fixed an issue where Doctor Strange could not use emotes in Doom Match.  [/p][p]2. Fixed incorrect display of Trusty Sidekick and Gifted Healer stats in Career custom data.  [/p][p]3. Fixed possible errors in displaying Career Highest Rank Eternity scores for All Seasons.  [/p][p]4. Fixed inconsistent In-Game Friends status updates in the team lobby.  [/p][p]5. Fixed rare cases where the Hero Ban feature might not function properly.  [/p][p] [/p][h2][b]Heroes[/b][/h2][p][/p][p]1. Strange's Portal Puzzle: Resolved a mystical mishap where Doctor Strange's portals could sometimes, under certain latency conditions, open up straight into the spawn room. Stephen's portals now stay on the right path!  [/p][p]2. Winter Soldier's Cold Cut: Fixed a rare bug where Winter Soldier's Ultimate Ability could be interrupted if Moon Knight's Ankh sent him flying at the exact moment of activation. Now, even Khonshu's knight can't freeze Bucky's finishing move ever AGAIN!  [/p][p]3. Jeff & Storm's Wild Ride: Addressed a quirk where the Team-Up between Jeff the Land Shark and Storm could, in extreme cases, send Jeff zooming at super-speed. The only thing breaking the sound barrier now is Storm's thunder—Jeff's back to making a splash, not a dash!  [/p][p] [/p][p] [/p][p]The Marvel Rivals universe is ever-evolving, and we have plenty more surprises and updates on the horizon. Stay tuned to our official announcements for more![/p]";
-
-const processed = bbobHTML(row, presetHTML5());
-
 export const dummyData = {
-  appnews: {
-    appid: 2767030,
-    newsitems: [
-      {
-        gid: "1801617199563047",
-        title: "Marvel Rivals Version 20250612 Patch Notes",
-        url: "https://steamstore-a.akamaihd.net/news/externalpost/steam_community_announcements/1801617199563047",
-        is_external_url: true,
-        author: "Marvel Rivals",
-        contents: processed,
-        feedlabel: "Community Announcements",
-        date: 1749575148,
-        feedname: "steam_community_announcements",
-        feed_type: 1,
-        appid: 2767030,
-      },
-    ],
-    count: 184,
-  },
-};
+  "appnews": {
+      "appid": 2767030,
+      "newsitems": [
+          {
+              "gid": "1802893906699152",
+              "title": "'The winds of change are blowing': After 159 days Marvel Rivals has finally nerfed Storm, albeit not by much",
+              "url": "https://steamstore-a.akamaihd.net/news/externalpost/PC Gamer/1802893906699152",
+              "is_external_url": true,
+              "author": "Elie Gould",
+              "contents": "<img src=\"https://cdn.mos.cms.futurecdn.net/b44s8hV8RRuLwftHWbopk.jpg\"/><br><br>\n                It's been about half a year since NetEase gave Storm a massive touch-up in <a href=\"https://www.pcgamer.com/uk/marvel-rivals/\" target=\"_blank\">Marvel Rivals</a>. A somewhat temporary fix to players not touching the X-Men hero in Season 0, when Season 1 came around the devs <a href=\"https://www.pcgamer.com/games/third-person-shooter/marvel-rivals-devs-have-accidentally-buffed-storm-into-a-tornado-yeeting-monster-that-everyone-is-picking-and-no-one-knows-how-to-counter-a-clustered-group-is-a-snack-for-us/\" target=\"_blank\">simply made her too strong to resist</a>...\n                <a href=\"https://www.pcgamer.com/games/third-person-shooter/the-winds-of-change-are-blowing-after-159-days-marvel-rivals-has-finally-nerfed-storm-albeit-not-by-much/?utm_source=steam&utm_medium=referral\" target=\"_blank\">Read more.</a>",
+              "feedlabel": "PC Gamer",
+              "date": 1750258006,
+              "feedname": "PC Gamer",
+              "feed_type": 0,
+              "appid": 2767030
+          },
+          {
+              "gid": "1802354289731116",
+              "title": "Marvel Rivals Version 20250619 Balance Post",
+              "url": "https://steamstore-a.akamaihd.net/news/externalpost/steam_community_announcements/1802354289731116",
+              "is_external_url": true,
+              "author": "Marvel Rivals",
+              "contents": "[p]Greeting Rivals![/p][p]Even the mightiest heroes need an adjustment every now and then. Whether it's providing a quick exit strategy, getting a moonlit boost, or making sure Jeff chews before he swallows! It's time for another round of heroic tweaks to keep the battlefield as epic (and balanced) as ever.[/p][p][/p][h1][b]ADJUSTMENTS[/b][/h1][p] [/p][h2][b]VANGUARD[/b][/h2][p] [/p][h3]Thor[/h3][p]Thor's ability just got a thunderous QOL upgrade![/p][list][*][p]Awakening Rune New Mechanic: After casting Awakening Rune, you can now manually cancel the Awakened state after a brief delay.[/p][/*][/list][p] [/p][h3][b]DUELIST[/b][/h3][p] [/p][h3]Moon Knight[/h3][p]Khonshu must be smiling. Mark's survivability gets a slight boost![/p][list][*][p]Increase base health from 250 to 275.[/p][/*][/list][p][b] [/b][/p][h3]Star-Lord[/h3][p]Time for Peter to slightly turn up the volume at mid-range![/p][list][*][p]Element Guns Damage Falloff: Adjust maximum Damage Falloff at 25m from 60% to 65%.[/p][/*][/list][p][b] [/b][/p][h3]Storm[/h3][p]The winds of change are blowing. We're slightly reducing the frequency of her Ultimate Ability.[/p][list][*][p]Increase energy cost for Omega Hurricane (Ultimate Ability) from 3100 to 3400.[/p][/*][/list][p] [/p][h2][b]TEAM-UP ABILITIES[/b][/h2][p] [/p][h3]Chilling Assault (Luna Snow- Hawkeye)[/h3][p]Time to cool Hawkeye's roll! We're reducing the power on his end of his Team-Up Ability.[/p][list][*][p]Ice Arrow Adjustment: Reduce damage from 30 (120 max at 30m) to 25 (100 max at 30m). Reduce the stun duration for enemies hit beyond 30m from 1.5s to 0.8s and Spell Field Damage range from 3m to 1m.[/p][/*][/list][p] [/p][h3]Jeff-nado (Storm - Jeff the Land Shark)[/h3][p]We're increasing the time it takes to swallow the competition because even Jeff needs to chew his food, folks![/p][list][*][p]Increase time required to devour enemies from 1.2s to 1.5s.[/p][/*][/list][p] [/p][h3]Rocket Network (Rocket Raccoon - Peni Parker)[/h3][p]Looks like there's going to be a bit of a delay in the network. We're reducing the usage coverage of this Team-Up.[/p][list][*][p]Increase Armored Spider-Nest cooldown from 20s to 30s.[/p][/*][/list]",
+              "feedlabel": "Community Announcements",
+              "date": 1750183358,
+              "feedname": "steam_community_announcements",
+              "feed_type": 1,
+              "appid": 2767030
+          },
+          {
+              "gid": "1802354289729726",
+              "title": "Marvel Rivals Version 20250619 Patch Notes",
+              "url": "https://steamstore-a.akamaihd.net/news/externalpost/steam_community_announcements/1802354289729726",
+              "is_external_url": true,
+              "author": "Marvel Rivals",
+              "contents": "[p]Greetings, Rivals![/p][p]We're thrilled to announce the upcoming patch drops on June 19, 2025, at 9: 00: 00 AM (UTC)! This update will also be deployed seamlessly, no server downtime required! Simply log in after the update and jump right back into the action. Here's what's coming in this patch:[/p][p] [/p][h1][b]Limited-Time Event: Venom's Bubble Pop[/b][/h1][p]The Venom's Bubble Pop event bursts onto the scene! Complete event missions to earn rewards, including the Human Torch: Future Foundation costume absolutely FREE![/p][p]Unlock the Premium edition to claim even more rewards, such as the Venom - Pink Bubble costume! Plus, once you unlock Premium, you gain permanent access to the event until all rewards are claimed. No time limit![/p][p]Event Duration: 2025/6/19, 09: 00: 00 ~ 2025/7/11, 09: 00: 00 (UTC)[/p][p][/p][p][img src=\"{STEAM_CLAN_IMAGE}/44948917/466cff8b937c949833a54ffce8de7306f301d6a3.png\"][/img][/p][p][/p][h1][b]New In Store[/b][/h1][p]1. Iron Fist - Binary Fist Bundle[/p][p]2. Moon Knight - Phoenix Knight Bundle[/p][p]Available from: 2025/6/20, 02: 00: 00 (UTC)[/p][p] [/p][h1][b]Twitch Drops[/b][/h1][p]As the blazing \"Ignite\" series tournament heats up, we're launching new Twitch Drops! Simply accumulate the required viewing time to claim your rewards.[/p][p]This round's drops include the Magik - Will of Galacta costume along with related bundle content.[/p][p]Drops Period: 2025/6/19, 12: 00: 00 ~ 2025/7/16, 09: 00: 00 (UTC)[/p][p] [/p][h1][b]New Additions and Adjustments[/b][/h1][p]1. You can now set permissions for players joining Tournament Room Custom Games mid-match.[/p][p][/p][h1][b]All Platforms[/b][/h1][p]1. Fixed an issue where Faction icons displayed incorrectly during pre-match loading in Replay and Spectate modes.[/p][p] [/p][h2][b]Heroes[/b][/h2][p]1. Wolverine's Wild Ride: We've put the lockdown on some of Logan's lunacy where his Ultimate Ability could incorrectly also send enemies with Unstoppable status flying! Now, heroes in this state will not be getting on the Wolverine Express.  [/p][p]2. Mister Fantastic's Fantastic Fumble: Fixed a rare occurring error where Reed's Flexible Elongation could sometimes incorrectly interact with the environment leading to self-inflicted terrain KOs worthy of a Baxter Building blooper reel. We've set his coordinates straight, no more accidental \"Fantastic Flops!\"  [/p][p]3. Hulk's Smash Sync: Squashed a bug in Hulk's Incredible Leap charge bar UI where the display did not match actual progress. Now his leap will land with proper gamma-powered precision!  [/p][p]4. Black Widow's Bite Restored: Fixed a bug where Natasha's Team-Up Ability Pulse Rifle didn't damage destructible objects. Now, when Widow bites, nobody (or thing) in her way stands a chance!  [/p][p]5. Ultron's Overclocked Orders: Debugged a ghost in the machine where sometimes performing certain actions after using Ultron's Imperative: Patch would cause it to glitch out and cancel the effect. Now his imperative is as unstoppable as his ambition.  [/p][p][/p][p]The Marvel Rivals universe is ever-evolving, and we have plenty more surprises and updates on the horizon. Stay tuned to our official announcements for more![/p][p][/p][p][/p]",
+              "feedlabel": "Community Announcements",
+              "date": 1750179755,
+              "feedname": "steam_community_announcements",
+              "feed_type": 1,
+              "appid": 2767030
+          },
+          {
+              "gid": "1801617199563047",
+              "title": "Marvel Rivals Version 20250612 Patch Notes",
+              "url": "https://steamstore-a.akamaihd.net/news/externalpost/steam_community_announcements/1801617199563047",
+              "is_external_url": true,
+              "author": "Marvel Rivals",
+              "contents": "[p]Greetings, Rivals![/p][p][/p][p]We're thrilled to announce that the upcoming patch drops on June 12, 2025, at 09:00:00 (UTC)! As usual, this update will be applied without server downtime—so you can dive right back into your epic battles as soon as the update is live![/p][p] [/p][p]Here's a look at what's coming in this patch:[/p][p] [/p][h1][b]616 Day Vault[/b][/h1][p][/p][p]To celebrate 616 Day and the larger Marvel Universe, we're launching the inaugural 616 Vault event on June 13th![/p][p]For one week only, enjoy the return of exclusive limited-time bundles:[/p][p]· Venom - Snow Symbiote Bundle[/p][p]· Magik - Frozen Demon Bundle[/p][p]· Rocket Raccoon - Wild Winter Bundle[/p][p]· Groot - Holiday Happiness Bundle[/p][p]· Hawkeye - Galactic Fangs Bundle[/p][p]· Captain America - Galactic Talon Bundle[/p][p]Event Duration: 2025/6/13 02:00:00 (UTC) ~ 2025/6/20 02:00:00 (UTC)[/p][p] [/p][h1][b]New In Store[/b][/h1][p][/p][p]1.  Invisible Woman - Future Foundation Bundle[/p][p]2. The Thing - Future Foundation Bundle[/p][p]Available from 2025/6/13, 02:00:00 (UTC)[/p][p] [/p][h1][b]Fixes[/b][/h1][h2][b]All Platforms[/b][/h2][p][/p][p]1. Fixed an issue where Doctor Strange could not use emotes in Doom Match.  [/p][p]2. Fixed incorrect display of Trusty Sidekick and Gifted Healer stats in Career custom data.  [/p][p]3. Fixed possible errors in displaying Career Highest Rank Eternity scores for All Seasons.  [/p][p]4. Fixed inconsistent In-Game Friends status updates in the team lobby.  [/p][p]5. Fixed rare cases where the Hero Ban feature might not function properly.  [/p][p] [/p][h2][b]Heroes[/b][/h2][p][/p][p]1. Strange's Portal Puzzle: Resolved a mystical mishap where Doctor Strange's portals could sometimes, under certain latency conditions, open up straight into the spawn room. Stephen's portals now stay on the right path!  [/p][p]2. Winter Soldier's Cold Cut: Fixed a rare bug where Winter Soldier's Ultimate Ability could be interrupted if Moon Knight's Ankh sent him flying at the exact moment of activation. Now, even Khonshu's knight can't freeze Bucky's finishing move ever AGAIN!  [/p][p]3. Jeff & Storm's Wild Ride: Addressed a quirk where the Team-Up between Jeff the Land Shark and Storm could, in extreme cases, send Jeff zooming at super-speed. The only thing breaking the sound barrier now is Storm's thunder—Jeff's back to making a splash, not a dash!  [/p][p] [/p][p] [/p][p]The Marvel Rivals universe is ever-evolving, and we have plenty more surprises and updates on the horizon. Stay tuned to our official announcements for more![/p]",
+              "feedlabel": "Community Announcements",
+              "date": 1749575148,
+              "feedname": "steam_community_announcements",
+              "feed_type": 1,
+              "appid": 2767030
+          },
+          {
+              "gid": "1800991756476937",
+              "title": "Marvel Rivals Version 20250605 Patch Notes",
+              "url": "https://steamstore-a.akamaihd.net/news/externalpost/steam_community_announcements/1800991756476937",
+              "is_external_url": true,
+              "author": "Marvel Rivals",
+              "contents": "Greetings, Rivals!\nWe're thrilled to announce that the upcoming patch drops on June 5, 2025, at 09:00 (UTC)! As usual, this update will be applied without server downtime—so you can dive right back into your epic battles as soon as the update is live!\n\nHere's a look at what's coming in this patch:\n\n[h3][b]All-New Experimental Mode - Ultron's Battle Matrix Protocol[/b][/h3]\nThis week, step into the metallic mind of Ultron in the all-new game mode: Ultron's Battle Matrix Protocol! Command and conquer from Ultron's perspective and outsmart your foes with machine-like precision.\nGameplay Duration: 2025/6/6, 09:00 ~ 2025/6/23, 09:00(UTC)\n\n[img]{STEAM_CLAN_IMAGE}/44948917/72a6eb38ffcdc69eab9ba0a559dbdc7bd154eeb7.png[/img]\n\n[h3][b]New In Store[/b][/h3]\n1.Jeff the Land Shark - Sunshine Land Shark Bundle\n2.Wolverine - Weapon X Bundle\nAvailable from 2025/6/6, 02:00 (UTC)\n\n[h3][b]Limited-Time Login Reward: Tic-Tac-Toe Spray[/b][/h3]\nLog in during the event period to claim your exclusive Tic-Tac-Toe Spray! Challenge your friends to a game of wits and strategy in the spawn room (or the heat of battle).\nClaim Period: 2025/6/5, 09:00~ 2025/6/12, 09:00(UTC)\n\n[h3][b]Optimizations[/b][/h3]\n1.Loki's Illusions can now perform emotes with the real Loki during battle. Mischief, multiplied!\n2. Players can now have up to 6 sprays at once on the battlefield.\n\n[h3][b]Fixes[/b][/h3]\nAll Platforms\n1.Enhanced the Jeff-Nado Team-Up Profile story.\n\n[h3][b]Maps and Modes[/b][/h3]\n1.Fixed certain terrain issues in the Hellfire Gala: Krakoa map where characters could become stuck or clip into strange locations.\n\nThe Marvel Rivals universe is ever-evolving, and we have plenty more surprises and updates on the horizon. Stay tuned to our official announcements for more!",
+              "feedlabel": "Community Announcements",
+              "date": 1748964928,
+              "feedname": "steam_community_announcements",
+              "feed_type": 1,
+              "appid": 2767030
+          }
+      ],
+      "count": 187
+  }
+}
+
--- app/api/steam-patch-test/route.ts
+++ app/api/steam-patch-test/route.ts
@@ -1,30 +1,50 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
+import bbobHTML from "@bbob/html";
+import presetHTML5 from "@bbob/preset-html5";
+
+import { dummyData } from "./dummy-data";
+
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-import { dummyData } from "./dummy-data";
 
 export async function POST() {
   // 테스트용 고정 데이터 (실제론 외부 API 호출 후 데이터 넣음)
   const data = dummyData;
 
-  const news = data.appnews.newsitems[0];
+  // 1. feed_type이 1인 항목만 필터링하고 bbobHTML로 파싱
+  const processedItems = data.appnews.newsitems
+    .filter(item => item.feed_type === 1)
+    .map(item => {
+      const parsedContent = bbobHTML(item.contents, presetHTML5());
+      const formatContent = parsedContent.replace('{STEAM_CLAN_IMAGE}', 'https://clan.cloudflare.steamstatic.com/images');
 
-  const { error } = await supabase.from("steam_patch_logs").insert({
-    app_id: data.appnews.appid.toString(),
-    app_gid: news.gid,
-    app_name: news.author,
-    title: news.title,
-    content: news.contents,
-    published_at: new Date(news.date * 1000).toISOString(),
+      return {
+        app_id: data.appnews.appid.toString(),
+        app_name: item.author,
+        app_gid: item.gid,
+        title: item.title,
+        content: formatContent,
+        published_at: new Date(item.date * 1000).toISOString(),
+        synced_at: new Date(Date.now() + 9 * 60 * 60 * 1000).toISOString()
+      };
+    });
+
+  // 2. Supabase에 insert - 중복 방지를 위해 upsert 사용
+  const { error } = await supabase
+  .from('steam_patch_logs')                    
+  .upsert(processedItems, {                     
+    onConflict: 'app_gid',                    
+    ignoreDuplicates: true                    
   });
 
   if (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    console.error('Error inserting patch logs:', error);
+    return NextResponse.json({ error: 'Failed to insert patch logs' }, { status: 500 });
   }
 
-  return NextResponse.json({ message: "Insert success" });
+  return NextResponse.json({ success: true });
 }
