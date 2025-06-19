562efd5ab13787e6562bd7b5a1c677ea257a9afe
justart-dev <hbd9425@gmail.com>
2025-06-20 00:05:51 +0900

    패치 내역 수정

--- app/api/steam-patch-logs/route.ts
+++ app/api/steam-patch-logs/route.ts
@@ -1,30 +1,54 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
+
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+interface SteamAppMetadata {
+  capsule_image: string;
+}
+
+interface DatabaseRow {
+  id: string;
+  title: string;
+  app_id: string;
+  app_gid: string;
+  app_name: string;
+  published_at: string;
+  steam_app_metadata: SteamAppMetadata;
+}
+
 export async function GET() {
   try {
     const { data, error } = await supabase
-  .from("steam_patch_logs")
-  .select(`
-    id,title,app_id,app_gid,app_name,published_at,feed_type,
-    steam_app_metadata (
-      capsule_image
-    )
-  `)
-  .eq("feed_type", 1)
-  .order("published_at", { ascending: false })
-  .limit(5);
-
+      .from("steam_patch_logs")
+      .select(`
+        id, title, app_id, app_gid, app_name, published_at,
+        steam_app_metadata (
+          capsule_image
+        )
+      `)
+      .order("published_at", { ascending: false })
+      .limit(5);
 
     if (error) {
       throw error;
     }
-    console.log('get logs data', data)
-    return NextResponse.json(data);
+
+    const formattedData = (data as unknown as DatabaseRow[]).map(item => {
+      console.log("[patched current log item]:", item);
+      return {
+        id: item.id,
+        app_name: item.app_name,
+        app_gid: item.app_gid,
+        title: item.title,
+        published_at: item.published_at,
+        capsule_image: item.steam_app_metadata.capsule_image
+      }
+    })
+    return NextResponse.json(formattedData);
   } catch (error) {
     console.error("Error fetching patch logs:", error);
     return NextResponse.json(
