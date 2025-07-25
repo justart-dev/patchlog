5910ba5c00181f03b71a4d1e970ebac1febf42cf
parklego <hbd9425@gmail.com>
2025-07-26 06:47:44 +0900

    패치내역 10개까지 보여주게 수정

--- app/api/steam-patch-logs/route.ts
+++ app/api/steam-patch-logs/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
-
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
@@ -24,20 +23,22 @@ export async function GET() {
   try {
     const { data, error } = await supabase
       .from("steam_patch_logs")
-      .select(`
+      .select(
+        `
         id, title, app_id, app_gid, app_name, published_at,
         steam_app_metadata (
           capsule_image
         )
-      `)
+      `
+      )
       .order("published_at", { ascending: false })
-      .limit(5);
+      .limit(10);
 
     if (error) {
       throw error;
     }
 
-    const formattedData = (data as unknown as DatabaseRow[]).map(item => {
+    const formattedData = (data as unknown as DatabaseRow[]).map((item) => {
       console.log("[patched current log item]:", item);
       return {
         id: item.id,
@@ -45,9 +46,9 @@ export async function GET() {
         app_gid: item.app_gid,
         title: item.title,
         published_at: item.published_at,
-        capsule_image: item.steam_app_metadata.capsule_image
-      }
-    })
+        capsule_image: item.steam_app_metadata.capsule_image,
+      };
+    });
     return NextResponse.json(formattedData);
   } catch (error) {
     console.error("Error fetching patch logs:", error);
