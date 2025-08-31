7a16731042713364708971e6e05b34b94b26051b
parklego <hbd9425@gmail.com>
2025-08-31 09:36:44 +0900

    마블 라이벌즈 Glossary proto

---
+++ app/utils/marvelGlossary.ts
@@ -0,0 +1,58 @@
+const skillMap: Record<string, string> = {
+  // 캡틴 아메리카
+  "Sentinel Strike": "방패 강타(좌클릭)",
+  "Super-Soldier Slam": "든든한 지원(좌클릭)",
+  "Living Legend": "절대 의지(우클릭)",
+  "Leading Dash": "진두 지휘(Shift)",
+  "Vibranium Energy Saw": "비브라늄 에너지 방패(E)",
+  "Liberty Rush": "파죽지세(F)",
+  "Freedom Charge": "자유의 부름(Q)",
+  "Captain's Spirit": "불굴의 캡틴(C)",
+  "Charged Aegis": "초전도 방패(C)",
+
+  // 아이언맨
+  "Repulsor Blast": "리펄서 블래스트(좌클릭)",
+  Unibeam: "유니 빔(우클릭)",
+  "Hyper-Velocity": "고속 부스터(Shift)",
+  "Armor Overdrive": "아머 오버드라이브(E)",
+  "Micro-Missile Barrage": "미사일 폭격(F)",
+  "Invincible Pulse Cannon": "무적 펄스 캐논(Q)",
+  "Nano Tech": "나노 기술(패시브)",
+  "Gamma Overdrive": "감마 오버드라이브(E)",
+
+  // 토르
+  Thorforce: "토르의 힘(패시브)",
+  "Mjolnir Bash": "묠니르 강격(좌클릭)",
+  "Hammer Throw": "해머 투척(우클릭)",
+  "Storm Surge": "라이트닝 스톰(Shift)",
+  "Lightning Realm": "천둥의 영역(E)",
+  "Awakening Rune": "룬의 각성(F)",
+  "God of Thunder": "천둥의 신(Q)",
+  "Odinson Reborn": "토르 부활(패시브)",
+  "Thunderous Deity": "신의 가호(패시브)",
+
+  // 라쿤
+  "Wild Crawl": "최전선 걷기(패시브)",
+  "Flying Ace": "에이스 파일럿(패시브)",
+  "Bombard Mode": "화력 폭격(좌클릭)",
+  "Repair Mode": "치유의 라이트 볼(우클릭)",
+  "Jetpack Dash": "로켓 대시(Shift)",
+  "B.R.B.": "부활 비콘(E)",
+  "C.Y.A.": "코스믹 에너지 증폭 체인(Q)",
+  "Old Friends": "오래된 사이(C)",
+  "Web Server": "웹 서버(E)",
+  "Ammo Invention": "무기상(Z)",
+
+  // 루나 스노우
+  "Smooth Skate": "아이스 셰이드(패시브)",
+  "Cryo Heart": "얼어붙은 심장(패시브)",
+  "Light & Dark Ice": "빛과 어둠의 교차(좌클릭)",
+  "Absolute Zero": "절대 영도(우클릭)",
+  "Ice Arts": "눈의 춤(Shift)",
+  "Share The Stage": "아이돌의 매력(E)",
+  "Fate of Both Worlds": "운명 교향곡(Q)",
+  "Lunar Chi": "다시 찾은 고향의 냉기(패시브)",
+  "Light & Shadow Karma": "빛과 그림자의 카르마(C)",
+  "Icy Disco": "눈의 노래(패시브)",
+  "Frozen Chi": "서늘한 남풍(C)",
+};
