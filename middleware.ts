ab8845bbe32c2a647797550dc0712b4f78650ed6
justart-dev <justart-dev@users.noreply.github.com>
2026-03-28 17:58:48 +0900

    Make tier page public

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/",
   "/patch(.*)",
+  "/tier(.*)",
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api(.*)",
