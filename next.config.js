cad6ecfeb567d8cb6ed232e2feb69c65a38e0c29
justart-dev <justart-dev@users.noreply.github.com>
2026-04-27 20:01:43 +0900

    SEO: robots 설정 단순화 및 noindex 이슈 해결 (clip-memo 프로젝트 참고)

--- next.config.js
+++ next.config.js
@@ -23,19 +23,6 @@ const nextConfig = {
       },
     ];
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-Robots-Tag',
-            value: 'index, follow',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
